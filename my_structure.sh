6c603157cf6d929666c31ca71cc86ef2d07c10ed
Rakibul Islam <150028649+RakibulIslam10@users.noreply.github.com>
2025-07-05 19:52:56 +0600

    Update my_structure.sh

--- my_structure.sh
+++ my_structure.sh
@@ -16,3 +16,28 @@ touch lib/constants/dimensions.dart
 touch lib/constants/sizes.dart
 
 echo "✅ Structure created successfully!"
+
+
+echo "<>..................................................Createting Your Code --------------------------------------------------------------------------------"
+
+cat <<EOF> lib/constants/sizes.dart
+
+import 'package:flutter/cupertino.dart';
+
+class RakibVaiDone extends StatefulWidget {
+  const RakibVaiDone({super.key});
+
+  @override
+  State<RakibVaiDone> createState() => _RakibVaiDoneState();
+}
+
+class _RakibVaiDoneState extends State<RakibVaiDone> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+EOF
+
+
