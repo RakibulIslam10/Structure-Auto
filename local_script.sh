3a4b7c041cd056c0619b1cd4758b016e97205635
Rakibul Islam <150028649+RakibulIslam10@users.noreply.github.com>
2025-07-17 18:30:23 +0600

    Update local_script.sh

--- local_script.sh
+++ local_script.sh
@@ -3,15 +3,15 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-read -p "প্রজেক্টের নাম দিন: " project_name
+read -p "Enter Your Project Name: " project_name
 
-echo -e "${GREEN}Flutter প্রজেক্ট তৈরি করা হচ্ছে...${NC}"
+echo -e "${GREEN}Flutter Project Crating Start...${NC}"
 flutter create $project_name
 cd $project_name
 
-echo -e "${GREEN}Custom structure বসানো হচ্ছে...${NC}"
+echo -e "${GREEN}Custom structure Add Your Custom Structure...${NC}"
 curl -sSL https://raw.githubusercontent.com/RakibulIslam10/Structure-Auto/main/my_structure.sh | bash
 
-echo -e "${GREEN}Android Studio ওপেন করা হচ্ছে...${NC}"
+echo -e "${GREEN}Android Studio Android Studio Opeing...${NC}"
 "/c/Program Files/Android/Android Studio1/bin/studio64.exe" .
-echo -e "${GREEN}সকল কাজ সম্পন্ন হয়েছে!${NC}"
+echo -e "${GREEN}All Is Okay Now You are Ready ...!${NC}"
