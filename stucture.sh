dd19e769bb65d431a787e42d1d32ed1e884029d3
Rakibul Islam <150028649+RakibulIslam10@users.noreply.github.com>
2025-07-18 17:19:54 +0600

    Update stucture.sh

--- stucture.sh
+++ stucture.sh
@@ -1,28 +1,32 @@
-#!/usr/bin/env bash
-sed -i 's/\r$//' "$0"
+#!/bin/bash
 
 BASE_DIR="lib"
 
-echo "📁 Creating Your Custom Structure ..."
+echo "📁 Creating Your Custom Stucture ..."
 
+# Bindings
 mkdir -p $BASE_DIR/bind
 touch $BASE_DIR/bind/home_binding.dart
 touch $BASE_DIR/bind/next_binding.dart
 touch $BASE_DIR/bind/splash_bindings.dart
 
+# Core folders
 mkdir -p $BASE_DIR/core/api
 mkdir -p $BASE_DIR/core/helpers
 mkdir -p $BASE_DIR/core/languages
 mkdir -p $BASE_DIR/core/themes
 mkdir -p $BASE_DIR/core/utils
 
+# Resources
 mkdir -p $BASE_DIR/res
 touch $BASE_DIR/res/assets.dart
 
+# Routes
 mkdir -p $BASE_DIR/routes
 touch $BASE_DIR/routes/pages.dart
 touch $BASE_DIR/routes/routes.dart
 
+# Views
 mkdir -p $BASE_DIR/views/firebase
 mkdir -p $BASE_DIR/views/home/controller
 mkdir -p $BASE_DIR/views/home/model
@@ -30,11 +34,33 @@ mkdir -p $BASE_DIR/views/home/screens
 mkdir -p $BASE_DIR/views/splash
 touch $BASE_DIR/views/splash/splash_screen.dart
 
+# Widgets
 mkdir -p $BASE_DIR/widgets
 touch $BASE_DIR/widgets/initial.dart
 
+# Main entry
+touch $BASE_DIR/main.dart
 
+echo "✅ Your Structure And All File Folder Created Successfully!"
+
+
+
+
+
+
+
+# FOLDER IN FILE
 touch $BASE_DIR/views/home/screens/navigation_screen_mobile.dart
 touch $BASE_DIR/views/home/screens/navigation_screen.dart
 
-echo "✅ Your Structure And All File Folder Created Successfully!"
+
+
+
+
+# CODE IN MY FILE 
+
+
+
+
+
+
