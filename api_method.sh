a0d41847afe4c56ad432169907389a2cf698a762
Rakibul Islam <150028649+RakibulIslam10@users.noreply.github.com>
2025-07-22 13:48:44 +0600

    Update api_method.sh

--- api_method.sh
+++ api_method.sh
@@ -6,10 +6,12 @@ BASE_DIR="lib"
 mkdir -p "$BASE_DIR/core/api/services"
 touch "$BASE_DIR/core/api/services/api_request.dart"
 
+echo "✅ Folder created. Writing Dart code..."
+
 cat <<"EOF" > "$BASE_DIR/core/api/services/api_request.dart"
 void main() {
   print('Hello from API file!');
 }
 EOF
 
-echo "✅ File created at $BASE_DIR/core/api/services/api_request.dart"
+echo "✅ Dart file written to: $BASE_DIR/core/api/services/api_request.dart"
